64712a7c9f5e3050265b9638f7988a8c9578918c
Amina Sagou <amina.sagou@gmail.Com>
2018-05-16 18:34:16 +0800

    ProjectMember model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516093229) do
+ActiveRecord::Schema.define(version: 20180516101551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "project_members", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_members_on_project_id"
+    t.index ["user_id"], name: "index_project_members_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.text "project_name"
@@ -47,4 +56,6 @@ ActiveRecord::Schema.define(version: 20180516093229) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "project_members", "projects"
+  add_foreign_key "project_members", "users"
 end
