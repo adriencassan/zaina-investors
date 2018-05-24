a0c470f44057e2c6f6b12178b8a830cd0ee18d33
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-05-24 10:15:34 +0800

    final commit for branch

--- app/models/projectmessage.rb
+++ app/models/projectmessage.rb
@@ -1,28 +1,4 @@
 class Projectmessage < ApplicationRecord
   belongs_to :user
   belongs_to :project
-
-
-
-
-  def create # POST
-    @project = Project.find(params[:id])
-    @user = current_user
-    @projectmessage = Projectmessage.new(message_params)
-    @projectmessage.project = @project
-    @projectmessage.user = @user
-    @projectmessage.date = Date.today
-    if @projectmessage.save
-      redirect_to project_path(@project)
-    else
-      #@ingredients = Ingredient.all - @cocktail.ingredients
-      #render 'cocktails/show'
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:projectmessage).permit(message)
-  end
 end
