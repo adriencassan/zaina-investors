e69af0902205ce82f3ef874be1da4734ddd66d67
Amina Sagou <amina.sagou@gmail.Com>
2018-06-02 06:05:30 +0800

    seed only with @zaina.io

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,55 +11,54 @@
 zaina1 = User.new(email: "admin@zaina.io", password: "password", role: "Zaina", first_name:"aminazaina", last_name:"sagouzaina" ,admin: true)
 zaina1.save!
 
-# profile = Profile.new(user_id: "1", first_name: zaina1.first_name, last_name: zaina1.last_name)
-# profile.save!
+profile = Profile.new(user_id: "1", first_name: zaina1.first_name, last_name: zaina1.last_name)
+profile.save!
 
-# zaina2 = User.new(email: "johnzaina@a.a", password: "password", role: "Zaina", first_name:"johnzaina", last_name:"smithzaina" ,admin: true)
-# zaina2.save!
+zaina2 = User.new(email: "johnzaina@zaina.io", password: "password", role: "Zaina", first_name:"johnzaina", last_name:"smithzaina" ,admin: true)
+zaina2.save!
 
-# profile = Profile.new(user_id: "2", first_name: zaina2.first_name, last_name: zaina2.last_name)
-# profile.save!
+profile = Profile.new(user_id: "2", first_name: zaina2.first_name, last_name: zaina2.last_name)
+profile.save!
 
-# # Users / Advisors
+# Users / Advisors
 
-# advisor1 = User.new(email: "aminaadvisor@a.a", password: "password", role: "Advisor", first_name:"aminaadvisor", last_name:"sagouadvisor")
-# advisor1.save!
+advisor1 = User.new(email: "aminaadvisor@zaina.io", password: "password", role: "Advisor", first_name:"aminaadvisor", last_name:"sagouadvisor")
+advisor1.save!
 
-# profile = Profile.new(user_id: "2", first_name: advisor1.first_name, last_name: advisor1.last_name)
-# profile.save!
+profile = Profile.new(user_id: "2", first_name: advisor1.first_name, last_name: advisor1.last_name)
+profile.save!
 
-# #Users / Entrepreneurs
-# entrepreneur1 = User.new(email: "aminaentrepreneur@a.a", password: "password", role: "Entrepreneur", first_name:"aminaentrepreneur", last_name:"sagouentrepreneur")
-# entrepreneur1.save!
+#Users / Entrepreneurs
+entrepreneur1 = User.new(email: "aminaentrepreneur@zaina.io"
 
-# profile = Profile.new(user_id: "3", first_name: entrepreneur1.first_name, last_name: entrepreneur1.last_name)
-# profile.save!
-
-
-# # projects
-
-# project = Project.new(project_name: "lucas project", user_id: 4 )
-# project.save!
-
-# # companies
-# company = Company.new(project_id: "1")
-# company.save!
-
-# #projectmembers (of lucas project)
-# projectmember = Projectmember.new(user_id: 1, project_id: 1)
-# projectmember.save!
+profile = Profile.new(user_id: "3", first_name: entrepreneur1.first_name, last_name: entrepreneur1.last_name)
+profile.save!
 
 
-# #investors
-# investor = Investor.new(name: "Investor1", project_id: 1, nextstep: "Send presentation", comment:"Should move quickly if interest confirmed", nature: "Private equity fund", geography: "France", contacted:"yes", contacts:"ceo john smith", )
-# investor.save!
+# projects
 
-# #messages
+project = Project.new(project_name: "lucas project", user_id: 4 )
+project.save!
 
-# 5.times do
+# companies
+company = Company.new(project_id: "1")
+company.save!
 
-# projectmessage = Projectmessage.new(message: "my test message", project: Project.first, date: Date.today, user: User.all.sample)
-# projectmessage.save!
-# end
+#projectmembers (of lucas project)
+projectmember = Projectmember.new(user_id: 1, project_id: 1)
+projectmember.save!
+
+
+#investors
+investor = Investor.new(name: "Investor1", project_id: 1, nextstep: "Send presentation", comment:"Should move quickly if interest confirmed", nature: "Private equity fund", geography: "France", contacted:"yes", contacts:"ceo john smith", )
+investor.save!
+
+#messages
+
+5.times do
+
+projectmessage = Projectmessage.new(message: "my test message", project: Project.first, date: Date.today, user: User.all.sample)
+projectmessage.save!
+end
 
 
