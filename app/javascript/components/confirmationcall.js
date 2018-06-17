2bdc19434324aaecee1222a92c2a907c02e37e1b
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-06-17 15:01:46 +0200

    add catch error to avoid unnecessary alert in consol when the element is not displayed in the current page

--- app/javascript/components/confirmationcall.js
+++ app/javascript/components/confirmationcall.js
@@ -1,11 +1,10 @@
 function confirmationCall() {
-
 const button = document.getElementById('confirmcall');
+if (button == null){ return}
 button.addEventListener('click', (event) => {
   event.target.style.color = "green";
   event.target.innerText = 'Call confirmed !';
   const id = event.currentTarget.getAttribute("data-project-id");
-  console.log(id)
   fetch(`/projects/${id}/confirm_call`, {
     method: "PATCH"
   });
