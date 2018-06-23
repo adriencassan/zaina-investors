24a4f926c02195df5cd693fdd2c76cf54cbef210
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-06-23 16:39:59 +0200

    add sectors_update method in model investor

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -14,4 +14,10 @@ class Investor < ApplicationRecord
   def sectors_secondary
     InvestorSector.where(investor: self).where(rank: 2).map { |a| a.sector }
   end
+
+  def sectors_update(sectors1,sectors2)
+    self.sectors.destroy_all
+    sectors1.each { |sector| InvestorSector.create(project: self, sector: Sector.find(sector.id), rank:1)}
+    sectors2.each { |sector| InvestorSector.create(project: self, sector: Sector.find(sector.id), rank:2)}
+  end
 end
