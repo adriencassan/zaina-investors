c8df3cece538785cdb4bd00f77f2c2c08307f9c1
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-07-02 18:15:41 +0200

    adapat form to new columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626052555) do
+ActiveRecord::Schema.define(version: 20180702154920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(version: 20180626052555) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "corp_value_min"
+    t.integer "corp_value_max"
+    t.string "target_criteria"
+    t.string "terms_financial"
+    t.string "terms_other"
   end
 
   create_table "users", force: :cascade do |t|
