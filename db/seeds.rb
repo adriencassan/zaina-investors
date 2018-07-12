7f04ecd89f8a7b7ee2a4c0852bb274885763a178
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-07-12 20:54:45 +0200

    new seed by Amina

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,33 +68,44 @@ InvestorNomenclature.create(type_nomenclature: 'sector', name: "Nouvelles techno
 InvestorNomenclature.create(type_nomenclature: 'sector', name: "Smart city" )
 
 # Zone ciblées
-InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Maroc')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Afrique du Nord')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Afrique subsaharienne')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Afrique')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Algérie')
-InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Tunisie')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Cameroun')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Côte d’Ivoire')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Egypte')
+InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Maroc')
 InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Sénégal')
+InvestorNomenclature.create(type_nomenclature: 'zone', name: 'Tunisie')
 
 
-# Type d'opération
-InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Debt')
-InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Prise de participation majoritaire')
-InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Prise de participation minoritaire')
-InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Prise de participation indifférente')
+# Type d'opération / stade de développement ciblé
+InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Capital Amorçage')
+InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Capital Développement')
+InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Capital Transmission')
+InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Rachat de positions intermédiaires')
+InvestorNomenclature.create(type_nomenclature: 'type_operation', name: 'Retournement')
 
 
 # Natures opérations
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital Amorçage')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital Risque')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital Développement')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital Transmission')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'LBO')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Mezzanine ')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Rachat de positions intermédiaires ')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Retournement')
-InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Garantie ')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Accompagnement')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Avance remboursable')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital - Participation majoritaire')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital - Participation minoritaire')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Capital - Participation majo. et mino.')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Cofinancement')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Dette')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Dette LBO')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Dette mezzanine')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Quasi-capital')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Garantie')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Prêt dhonneur')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Prêt participatif')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Restructuration financière')
+InvestorNomenclature.create(type_nomenclature: 'nature_operation', name: 'Subvention')
+
+# Subvention, Prêt d'honneur, Avance remboursable, Prêt participatif, Garantie, Cofinancement, Accompagnement, restructuration financière
+
+
 
