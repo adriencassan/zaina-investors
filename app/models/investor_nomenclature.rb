6892e1c7e6785529eabf9e57e1a5fd818d7cbe35
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-07-12 11:23:15 +0200

    order by name the arrays returns in InvestorNomenclature

--- app/models/investor_nomenclature.rb
+++ app/models/investor_nomenclature.rb
@@ -1,18 +1,18 @@
 class InvestorNomenclature < ApplicationRecord
 
   def self.sectors
-    InvestorNomenclature.where(type_nomenclature: "sector")
+    InvestorNomenclature.where(type_nomenclature: "sector").order(:name)
   end
 
   def self.zones
-    InvestorNomenclature.where(type_nomenclature: "zone")
+    InvestorNomenclature.where(type_nomenclature: "zone").order(:name)
   end
 
   def self.nature_operations
-    InvestorNomenclature.where(type_nomenclature: "nature_operation").pluck(:name)
+    InvestorNomenclature.where(type_nomenclature: "nature_operation").order(:name).pluck(:name)
   end
 
   def self.type_operations
-    InvestorNomenclature.where(type_nomenclature: "type_operation").pluck(:name)
+    InvestorNomenclature.where(type_nomenclature: "type_operation").order(:name).pluck(:name)
   end
 end
