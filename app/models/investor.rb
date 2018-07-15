2ca0d8f8b0aedf23b9e6c03775400b7dfe983f72
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-07-15 14:00:00 +0200

    initialize export csv method

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -27,4 +27,32 @@ class Investor < ApplicationRecord
     sectors2.each { |id| InvestorAttribute.create(investor: self, investor_nomenclature: InvestorNomenclature.find(id), type_attribute: "sector_secondary")} unless sectors2.nil?
     zones.each { |id| InvestorAttribute.create(investor: self, investor_nomenclature: InvestorNomenclature.find(id), type_attribute: "zone")} unless zones.nil?
   end
+
+  def self.to_csv
+    CSV.generate({headers: true, col_sep: ";", encoding: 'UTF-8'}) do |csv|
+      csv << ["id","nom","nature","investment_min","investment_max","operation_type","localisation","comment","corp_value_min","corp_value_max","target_criteria","terms_financial","terms_other","sectors_primary","sectors_secondary","zones"]
+
+      all.each do |i|
+        csv << [
+          i.id,
+          i.name,
+          i.nature,
+          i.investment_min,
+          i.investment_max,
+          i.operation_type,
+          i.localisation,
+          i.comment.nil? ? "" : i.comment.gsub(/;/,","),
+          i.corp_value_min,
+          i.corp_value_max,
+          i.target_criteria.nil? ? "" : i.target_criteria.gsub(/;/,","),
+          i.terms_financial.nil? ? "" : i.terms_financial.gsub(/;/,","),
+          i.terms_other.nil? ? "" : i.terms_other.gsub(/;/,","),
+          i.sectors_primary.nil? ? "" : i.sectors_primary.pluck(:name).join("/"),
+          i.sectors_secondary.nil? ? "" : i.sectors_secondary.pluck(:name).join("/"),
+          i.zones.nil? ? "" : i.zones.pluck(:name).join("/")
+          ]
+      end
+    end
+  end
+
 end
