4008fcd6ff92062b613bff8f3ca652fbb268643b
Adrien Cassan <adrien.cassan.tech@gmail.com>
2018-07-15 10:20:02 +0200

    add export route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "investors#index"
 
   resources :investors, only: [:index]
+  get 'investors/export', to: "investors#export", defaults: { format: 'csv' }
 
   resources :investors do
      resources :investor_sectors
